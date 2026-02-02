b7cc43808d4b5ae120c950eb37cac5c74e4db322
Gergely Marton <wwworkingprocess@gmail.com>
2026-02-02 08:48:21 -0500

    fixed pylint issues

--- server.py
+++ server.py
@@ -1,3 +1,5 @@
+'''This application is using emotion detection to evaluate sentences
+'''
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -11,7 +13,7 @@ def render_index_page():
     return render_template('index.html')
 
 @app.route("/emotionDetector")
-def emotionDetector():
+def emotion_detection():
     ''' This function formats the output after calling the detection
     '''
     # Retrieve the text to analyze from the request arguments
@@ -20,7 +22,7 @@ def emotionDetector():
     # Pass the text to the emotion_detector function and store the response
     r = emotion_detector(text_to_analyze)
 
-    if (r['dominant_emotion'] is None):
+    if r['dominant_emotion'] is None:
         return 'Invalid text! Please try again!'
 
     text = f'''
